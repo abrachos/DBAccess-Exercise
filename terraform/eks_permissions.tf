48a972d550131640800aeed8a9b5a800f4af0a78
abrachos <anderson.bracho@gmail.com>
2025-01-14 12:05:49 -0400

    Add aws credentials to kube and deploy.yaml28

--- terraform/eks_permissions.tf
+++ terraform/eks_permissions.tf
@@ -1,29 +1,14 @@
 data "aws_iam_user" "github_action_user" {
-  user_name = "githubaction_aws"  # Reemplaza con el nombre del usuario IAM
+  user_name = "githubaction_aws"
 }
 
-resource "aws_eks_access_entry" "github_action_user" {
-  cluster_name  = "virtualaccount-cluster"  # Reemplaza con el nombre de tu clúster EKS
-  principal_arn = "arn:aws:iam::509399606070:user/githubaction_aws"  # ARN del usuario proporcionado
-  type          = "STANDARD"
+resource "aws_iam_role_policy_attachment" "github_action_user_AmazonEKSAdminPolicy" {
+  role       = data.aws_iam_user.github_action_user.user_name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSAdminPolicy"
 }
 
-resource "aws_eks_access_policy_association" "github_action_user_AmazonEKSAdminPolicy" {
-  cluster_name  = "virtualaccount-cluster"  # Reemplaza con el nombre de tu clúster EKS
-  policy_arn    = "arn:aws:iam::aws:policy/AmazonEKSAdminPolicy"
-  principal_arn = aws_eks_access_entry.github_action_user.principal_arn
-
-  access_scope {
-    type = "cluster"
-  }
+resource "aws_iam_role_policy_attachment" "github_action_user_AmazonEKSClusterAdminPolicy" {
+  role       = data.aws_iam_user.github_action_user.user_name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_eks_access_policy_association" "github_action_user_AmazonEKSClusterAdminPolicy" {
-  cluster_name  = "virtualaccount-cluster"  # Reemplaza con el nombre de tu clúster EKS
-  policy_arn    = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  principal_arn = aws_eks_access_entry.github_action_user.principal_arn
-
-  access_scope {
-    type = "cluster"
-  }
-}
