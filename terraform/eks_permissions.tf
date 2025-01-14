e298f9ed0823dcc905938d88c585c2e8f109856d
abrachos <anderson.bracho@gmail.com>
2025-01-14 12:28:03 -0400

    Add aws credentials to kube and deploy.yaml32

--- terraform/eks_permissions.tf
+++ terraform/eks_permissions.tf
@@ -2,12 +2,17 @@ data "aws_iam_role" "github_action_role" {
   name = "githubactiontoaws"
 }
 
-resource "aws_iam_role_policy_attachment" "github_action_role_AmazonEKSAdminPolicy" {
-  role       = data.aws_iam_role.github_action_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSAdminPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "github_action_role_AmazonEKSClusterAdminPolicy" {
+resource "aws_iam_role_policy_attachment" "github_action_role_AmazonEKSClusterPolicy" {
   role       = data.aws_iam_role.github_action_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "github_action_role_AmazonEKSWorkerNodePolicy" {
+  role       = data.aws_iam_role.github_action_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "github_action_role_AmazonEC2ContainerRegistryReadOnly" {
+  role       = data.aws_iam_role.github_action_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
