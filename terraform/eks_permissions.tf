64cd61e529cf0b0b8bfe2a1cecd7141ff8669f86
abrachos <anderson.bracho@gmail.com>
2025-01-15 12:37:32 -0400

    Adding configuration for GHA17

--- terraform/eks_permissions.tf
+++ terraform/eks_permissions.tf
@@ -17,10 +17,9 @@ resource "aws_iam_role_policy_attachment" "github_action_role_AmazonEC2Container
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "kubernetes_role" "eks_role" {
+resource "kubernetes_cluster_role" "eks_role" {
   metadata {
-    name      = "eks-role"
-    namespace = "default"
+    name = "github-actions-role"
   }
 
   rule {
@@ -28,29 +27,22 @@ resource "kubernetes_role" "eks_role" {
     resources  = ["nodes", "pods", "services"]
     verbs      = ["get", "list", "watch"]
   }
-
-  lifecycle {
-    ignore_changes = [
-      metadata[0].annotations["kubectl.kubernetes.io/last-applied-configuration"]
-    ]
-  }
 }
 
-resource "kubernetes_role_binding" "eks_rolebinding" {
+resource "kubernetes_cluster_role_binding" "eks_rolebinding" {
   metadata {
-    name      = "eks-rolebinding"
-    namespace = "default"
+    name = "github-actions-rolebinding"
   }
 
   subject {
     kind      = "User"
-    name      = "arn:aws:sts::509399606070:assumed-role/githubactiontoaws/GitHubActions"
+    name      = "arn:aws:eks:us-east-2:509399606070:cluster/virtualaccount-cluster"
     api_group = "rbac.authorization.k8s.io"
   }
 
   role_ref {
-    kind      = "Role"
-    name      = kubernetes_role.eks_role.metadata[0].name
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.eks_role.metadata[0].name
     api_group = "rbac.authorization.k8s.io"
   }
 }
