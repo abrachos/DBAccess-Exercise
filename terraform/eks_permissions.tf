63e194bae0aea51a95f18a370e776dd3a817d31d
abrachos <anderson.bracho@gmail.com>
2025-01-14 22:08:25 -0400

    Adding configurations to communicate with aws from GHA

--- terraform/eks_permissions.tf
+++ terraform/eks_permissions.tf
@@ -16,3 +16,35 @@ resource "aws_iam_role_policy_attachment" "github_action_role_AmazonEC2Container
   role       = data.aws_iam_role.github_action_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+resource "kubernetes_role" "eks_role" {
+  metadata {
+    name      = "eks-role"
+    namespace = "default"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["nodes", "pods", "services"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_role_binding" "eks_rolebinding" {
+  metadata {
+    name      = "eks-rolebinding"
+    namespace = "default"
+  }
+
+  subject {
+    kind      = "User"
+    name      = "arn:aws:sts::509399606070:assumed-role/githubactiontoaws/GitHubActions"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  role_ref {
+    kind      = "Role"
+    name      = kubernetes_role.eks_role.metadata[0].name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
---
+++ terraform/providers.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "eks"
+  region = "us-east-2"
+  read_timeout  = "60s"
+  write_timeout = "60s"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = "virtualaccount-cluster"
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.aws_eks_cluster.cluster.name
+}
