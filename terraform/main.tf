67d6993efe6ee0d781e323858db54138d0b3c130
abrachos <anderson.bracho@gmail.com>
2025-01-15 00:50:53 -0400

    Adding configuration for GHA4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"  # Puedes cambiar la versión según tus necesidades
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-00cae169b96dabf0d" 
+  ami           = "ami-00cae169b96dabf0d"
   instance_type = "t2.micro"
 
   tags = {
@@ -15,7 +24,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "13.15" 
+  engine_version       = "13.15"
   instance_class       = "db.m5d.large"
   identifier           = "clientes-db"
   username             = "postgres"
@@ -27,4 +36,3 @@ resource "aws_db_instance" "default" {
     Name = "database-instance"
   }
 }
-
