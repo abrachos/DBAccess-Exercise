703c8b8402061ef4d36d9c3198402e0c32cc2ab5
abrachos <anderson.bracho@gmail.com>
2025-01-16 14:12:52 -0400

    Adding configuration for GHA60

--- VirtualAccount/app.py
+++ VirtualAccount/app.py
@@ -15,7 +15,7 @@ from models import Client
 # Index
 @app.route("/")
 def index():
-    return "This is the app index2123"
+    return "This is the app index"
 
 # Add client and money
 @app.route("/add")
